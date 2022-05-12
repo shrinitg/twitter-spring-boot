841af09a4f1046d27fc847cb6d48bdfd95d0f883
Shrinit Goyal <shrinitgoyal@Shrinits-MacBook-Air.local>
2022-05-12 14:26:51 +0530

    initial commit

---
+++ src/main/java/com/example/twitterTest/TwitterTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.twitterTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterTestApplication.class, args);
+	}
+
+}
